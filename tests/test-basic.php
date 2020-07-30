a3b56f02d5d290ccdc384e3be21aa66fa590da5d
thingsym <thingsym@gmail.com>
2020-07-30 16:07:40 +0900

    test: fix test case

--- tests/test-basic.php
+++ tests/test-basic.php
@@ -20,8 +20,9 @@ class Test_Custom_Post_Type_Widgets_Basic extends WP_UnitTestCase {
 	 * @group basic
 	 */
 	function constructor() {
-		$this->assertEquals( 10, has_action( 'plugins_loaded', array( $this->custom_post_type_widgets, 'load' ) ) );
-		$this->assertEquals( 10, has_action( 'widgets_init', array( $this->custom_post_type_widgets, 'init' ) ) );
+		$this->assertEquals( 10, has_filter( 'init', array( $this->custom_post_type_widgets, 'load_textdomain' ) ) );
+		$this->assertEquals( 10, has_filter( 'plugins_loaded', array( $this->custom_post_type_widgets, 'load' ) ) );
+		$this->assertEquals( 10, has_filter( 'widgets_init', array( $this->custom_post_type_widgets, 'init' ) ) );
 	}
 
 	/**
@@ -37,6 +38,8 @@ class Test_Custom_Post_Type_Widgets_Basic extends WP_UnitTestCase {
 	 * @group basic
 	 */
 	function init() {
+		$this->custom_post_type_widgets->init();
+
 		global $wp_widget_factory;
 
 		$this->assertArrayHasKey( 'WP_Custom_Post_Type_Widgets_Recent_Posts', $wp_widget_factory->widgets );
@@ -46,6 +49,15 @@ class Test_Custom_Post_Type_Widgets_Basic extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'WP_Custom_Post_Type_Widgets_Recent_Comments', $wp_widget_factory->widgets );
 		$this->assertArrayHasKey( 'WP_Custom_Post_Type_Widgets_Tag_Cloud', $wp_widget_factory->widgets );
 		$this->assertArrayHasKey( 'WP_Custom_Post_Type_Widgets_Search', $wp_widget_factory->widgets );
+	}
+
+	/**
+	 * @test
+	 * @group basic
+	 */
+	public function load_textdomain() {
+		$result = $this->custom_post_type_widgets->load_textdomain();
+		$this->assertNull( $result );
 	}
 
 	/**
