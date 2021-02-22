acc04c9a2f0c762bc000c025724cdd662aed9be6
thingsym <thingsym@gmail.com>
2021-02-22 16:46:32 +0900

    test: add test case

--- tests/test-widget-custom-post-type-archive.php
+++ tests/test-widget-custom-post-type-archive.php
@@ -122,7 +122,28 @@ class Test_WP_Custom_Post_Type_Widgets_Archives extends WP_UnitTestCase {
 	 * @group wp_custom_post_type_widgets_archives
 	 */
 	function get_month_link_custom_post_type() {
-		$this->markTestIncomplete( 'This test has not been implemented yet.' );
+		$this->_register_post_type();
+
+		$new_instance = array(
+			'title'          => 'aaaaa',
+			'posttype'       => 'test',
+			'count'          => false,
+			'dropdown'       => false,
+		);
+
+		$this->wp_custom_post_type_widgets_archives->update( $new_instance, array() );
+
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure( '/archives/%post_id%' );
+
+		$this->_register_sidebar_widget( 3, $new_instance );
+
+		$expected = 'http://example.org/archives/test/date/2019/08';
+
+		$url = 'http://example.org/archives/date/2020/02';
+		$actual = $this->wp_custom_post_type_widgets_archives->get_month_link_custom_post_type( $url, '2019', '08' );
+
+		$this->assertEquals( $expected, $actual );
 	}
 
 	/**
@@ -130,7 +151,115 @@ class Test_WP_Custom_Post_Type_Widgets_Archives extends WP_UnitTestCase {
 	 * @group wp_custom_post_type_widgets_archives
 	 */
 	function trim_post_type() {
-		$this->markTestIncomplete( 'This test has not been implemented yet.' );
+		$new_instance = array(
+			'title'          => 'aaaaa',
+			'posttype'       => 'test',
+			'count'          => false,
+			'dropdown'       => false,
+		);
+
+		$this->wp_custom_post_type_widgets_archives->update( $new_instance, array() );
+
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure( '/archives/%post_id%' );
+
+		$this->_register_sidebar_widget( 3, $new_instance );
+
+		$expected = 'http://example.org/archives/test/date/2019/08';
+
+		$url = 'http://example.org/archives/test/date/2019/08?post_type=test';
+		$actual = $this->wp_custom_post_type_widgets_archives->trim_post_type( $url );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * @test
+	 * @group wp_custom_post_type_widgets_archives
+	 */
+	function trim_post_type_filter() {
+		$new_instance = array(
+			'title'          => 'aaaaa',
+			'posttype'       => 'test',
+			'count'          => false,
+			'dropdown'       => false,
+		);
+
+		$this->wp_custom_post_type_widgets_archives->update( $new_instance, array() );
+
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure( '/archives/%post_id%' );
+
+		$this->_register_sidebar_widget( 3, $new_instance );
+
+		add_filter( 'custom_post_type_widgets/archive/trim_post_type', array( $this, '_filter_trim_post_type' ), 10, 3 );
+
+		$expected = 'http://example.org/archives/test/date/2019/08?post_type=abc';
+
+		$url = 'http://example.org/archives/test/date/2019/08?post_type=test';
+		$actual = $this->wp_custom_post_type_widgets_archives->trim_post_type( $url );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * @test
+	 * @group wp_custom_post_type_widgets_archives
+	 */
+	function _filter_trim_post_type( $new_link_html, $link_html, $posttype ) {
+		$this->assertEquals( $new_link_html, 'http://example.org/archives/test/date/2019/08' );
+		$this->assertEquals( $link_html, 'http://example.org/archives/test/date/2019/08?post_type=test' );
+		$this->assertEquals( $posttype, 'test' );
+
+		$new_link_html = $new_link_html . '?post_type=abc';
+
+		return $new_link_html;
+	}
+
+	/**
+	 * Utility
+	 */
+	function _register_sidebar_widget( $id, $new_option ) {
+		$this->wp_custom_post_type_widgets_archives->number = $id;
+		$options  = get_option( $this->wp_custom_post_type_widgets_archives->option_name );
+		$options[ $id ] = $new_option;
+		update_option( $this->wp_custom_post_type_widgets_archives->option_name, $options );
+	}
+
+	/**
+	 * Utility
+	 */
+	function _register_post_type() {
+		$labels = [
+			"name" => "test",
+			"singular_name" => "test",
+		];
+
+		$args = [
+			"label" => "test",
+			"labels" => $labels,
+			"description" => "",
+			"public" => true,
+			"publicly_queryable" => true,
+			"show_ui" => true,
+			"delete_with_user" => false,
+			"show_in_rest" => true,
+			"rest_base" => "",
+			"rest_controller_class" => "WP_REST_Posts_Controller",
+			"has_archive" => true,
+			"show_in_menu" => true,
+			"show_in_nav_menus" => true,
+			"delete_with_user" => false,
+			"exclude_from_search" => false,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => false,
+			"rewrite" => [ "slug" => "test", "with_front" => true ],
+			"query_var" => true,
+			"supports" => [ "title", "editor", "thumbnail", "comments" ],
+		];
+
+		register_post_type( "test", $args );
 	}
 
 }
--- tests/test-widget-custom-post-type-calendar.php
+++ tests/test-widget-custom-post-type-calendar.php
@@ -124,7 +124,74 @@ class Test_wp_custom_post_type_widgets_calendar extends WP_UnitTestCase {
 	 * @group wp_custom_post_type_widgets_calendar
 	 */
 	function get_month_link_custom_post_type() {
-		$this->markTestIncomplete( 'This test has not been implemented yet.' );
+		$this->_register_post_type();
+
+		$new_instance = array(
+			'title'          => 'aaaaa',
+			'posttype'       => 'test',
+			'count'          => false,
+			'dropdown'       => false,
+		);
+
+		$this->wp_custom_post_type_widgets_calendar->update( $new_instance, array() );
+
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure( '/archives/%post_id%' );
+
+		$this->_register_sidebar_widget( 3, $new_instance );
+
+		$expected = 'http://example.org/archives/test/date/2019/08';
+
+		$url = 'http://example.org/archives/date/2020/02';
+		$actual = $this->wp_custom_post_type_widgets_calendar->get_month_link_custom_post_type( $url, '2019', '08' );
+
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Utility
+	 */
+	function _register_sidebar_widget( $id, $new_option ) {
+		$this->wp_custom_post_type_widgets_calendar->number = $id;
+		$options  = get_option( $this->wp_custom_post_type_widgets_calendar->option_name );
+		$options[ $id ] = $new_option;
+		update_option( $this->wp_custom_post_type_widgets_calendar->option_name, $options );
+	}
+
+	/**
+	 * Utility
+	 */
+	function _register_post_type() {
+		$labels = [
+			"name" => "test",
+			"singular_name" => "test",
+		];
+
+		$args = [
+			"label" => "test",
+			"labels" => $labels,
+			"description" => "",
+			"public" => true,
+			"publicly_queryable" => true,
+			"show_ui" => true,
+			"delete_with_user" => false,
+			"show_in_rest" => true,
+			"rest_base" => "",
+			"rest_controller_class" => "WP_REST_Posts_Controller",
+			"has_archive" => true,
+			"show_in_menu" => true,
+			"show_in_nav_menus" => true,
+			"delete_with_user" => false,
+			"exclude_from_search" => false,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => false,
+			"rewrite" => [ "slug" => "test", "with_front" => true ],
+			"query_var" => true,
+			"supports" => [ "title", "editor", "thumbnail", "comments" ],
+		];
+
+		register_post_type( "test", $args );
 	}
 
 }
